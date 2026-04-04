63e58c75e159f4ced7540b3559882a57d737dd88
Wagner Silva <249521811+Wagner-Silva-Dev@users.noreply.github.com>
2026-04-04 19:29:15 -0300

    feat: adiciona classe CalculaParcelas que exibe opções de parcelamento acima de R$ 1000

---
+++ fundamentos/lacosrepeticao/CalculaParcelas.java
@@ -0,0 +1,23 @@
+package fundamentos.lacosrepeticao;
+
+import java.util.Scanner;
+
+public class CalculaParcelas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o valor total: ");
+        double total = scanner.nextDouble();
+
+        for (int parcelas = 1; parcelas <= total; parcelas ++) {
+            double valorParcelas = total / parcelas;
+
+            if (valorParcelas < 1000){
+                break;
+            }
+
+            System.out.println(parcelas + " parcelas de R$ " + valorParcelas);
+        }
+
+    }
+}
