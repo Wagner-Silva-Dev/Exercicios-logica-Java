dd24cec020f2adc173b8f8535fca5e8dde090062
Wagner Silva <249521811+Wagner-Silva-Dev@users.noreply.github.com>
2026-04-06 21:33:04 -0300

    feat: adiciona classe AprendendoArrays com exemplo básico de arrays em Java

---
+++ fundamentos/arrays/AprendendoArrays.java
@@ -0,0 +1,26 @@
+package fundamentos.arrays;
+
+public class AprendendoArrays {
+//    Antes dos arrays
+//    public static void main(String[] args) {
+//        int idade1 = 25;
+//        int idade2 = 40;
+//        int idade3 = 55;
+//
+//        System.out.println(idade1);
+//        System.out.println(idade2);
+//        System.out.println(idade3);
+//
+//    }
+//}
+    public static void main(String[] args) {
+        int []idades = new  int[3];
+        idades[0] = 25;
+        idades[1] = 40;
+        idades[2] = 55;
+
+        for (int i = 0; i < idades.length; i++) {
+            System.out.println( idades[i] );
+        }
+    }
+}
