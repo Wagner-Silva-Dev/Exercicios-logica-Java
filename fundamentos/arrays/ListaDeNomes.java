8593f87b0b98756945937ef6f03c6891d8b3a94d
Wagner Silva <249521811+Wagner-Silva-Dev@users.noreply.github.com>
2026-04-08 18:55:04 -0300

    refactor: substitui arrays separados por array bidimensional em ListaDeNomes

--- fundamentos/arrays/ListaDeNomes.java
+++ fundamentos/arrays/ListaDeNomes.java
@@ -2,23 +2,12 @@ package fundamentos.arrays;
 
 public class ListaDeNomes {
     public static void main(String[] args) {
-        String[] nomes = new String[5];
+        String[][] turma = {{"Ana", "8.5"}, {"Bruno", "6.0"}, {"Carlos", "9.5"}, {"Daniela", "4.0"}, {"Eduardo", "7.5"}
+        };
 
-        nomes[0] = "Ana";
-        nomes[1] = "Bruno";
-        nomes[2] = "Carlos";
-        nomes[3] = "Daniela";
-        nomes[4] = "Eduardo";
 
-        double[] notas = new double[5];
-        notas[0] = 8.5;
-        notas[1] = 6.0;
-        notas[2] = 9.5;
-        notas[3] = 4.0;
-        notas[4] = 7.5;
-
-        for (int contador = 0; contador < nomes.length; contador++) {
-            System.out.println(nomes[contador] + " - "  + notas[contador]);
+        for (int contador = 0; contador < turma.length; contador++) {
+            System.out.println(turma[contador][0]+ " - " + turma[contador][1]);
         }
     }
 }
