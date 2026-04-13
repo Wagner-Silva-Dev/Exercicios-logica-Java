64b9993932896df5dfb636f6fb09e548118cc652
Wagner Silva <249521811+Wagner-Silva-Dev@users.noreply.github.com>
2026-04-12 22:46:19 -0300

    refactor: extrai impressão de especificações para método em Carro

--- fundamentos/classes/concessionaria/Carro.java
+++ fundamentos/classes/concessionaria/Carro.java
@@ -6,4 +6,11 @@ public class Carro {
     public String modelo;
 
     public int ano;
+    
+    public void imprimirEspecificacoesCarro(){
+        System.out.println("Especificações do carro");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Ano: " + this.ano);
+    }
 }
