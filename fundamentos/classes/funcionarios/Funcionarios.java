129bd1f8dac0dd75b7d3e6d0b028a790a7129096
Wagner Silva <249521811+Wagner-Silva-Dev@users.noreply.github.com>
2026-04-13 22:04:59 -0300

    feat: adiciona classes Funcionarios, ImprimirFuncionarios e FuncionariosTest com cadastro e média salarial

---
+++ fundamentos/classes/funcionarios/Funcionarios.java
@@ -0,0 +1,7 @@
+package fundamentos.classes.funcionarios;
+
+public class Funcionarios {
+    public String []nomes = new String[3];
+    public int []idades = new int[3];
+    public double []salarios = new double[3];
+}
---
+++ fundamentos/classes/funcionarios/ImprimirFuncionarios.java
@@ -0,0 +1,22 @@
+package fundamentos.classes.funcionarios;
+
+public class ImprimirFuncionarios {
+    public void imprimir(Funcionarios funcionarios) {
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Funcionario " + (i + 1) + ":");
+            System.out.println("Nome: " + funcionarios.nomes[i]);
+            System.out.println("Idade: " + funcionarios.idades[i]);
+            System.out.printf("Salario: %.2f\n", funcionarios.salarios[i]);
+            System.out.println("----------------------------");
+        }
+    }
+    public void imprimirMedia(Funcionarios funcionarios) {
+        double soma = 0;
+        for (int i = 0; i < funcionarios.salarios.length; i++) {
+            soma += funcionarios.salarios[i];
+        }
+        double media = soma / funcionarios.salarios.length;
+        System.out.printf("A média dos salarios é de %.2f", media);
+    }
+}
