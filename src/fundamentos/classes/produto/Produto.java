88dbe8b536c3b2ebc51f29b47e27acdd3205a65e
Wagner Silva <249521811+Wagner-Silva-Dev@users.noreply.github.com>
2026-04-27 19:34:21 -0300

    feat: adiciona classe Produto com campos encapsulados

---
+++ src/fundamentos/classes/produto/Produto.java
@@ -0,0 +1,40 @@
+package fundamentos.classes.produto;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private int estoque;
+
+    public void imprimirDados() {
+        System.out.println("-------------------------");
+        System.out.println("Relatório do produto");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Estoque: " + this.estoque);
+        System.out.println("Preço: " + this.preco);
+        System.out.println("-----------------------------");
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+}
